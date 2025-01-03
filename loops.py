2546adf40ffcfdb189e1d807e068e9566ebb906d
byasalgal <122166499+byasalgal@users.noreply.github.com>
2025-01-03 19:47:47 +0800

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,9 @@
+iin = int(input("enter number: "))
+for i in range(1,11):
+    print(iin ,"*", i, "=" ,iin * i)
+
+string = input("enter string: ")
+string2 = ("")
+for i in string:
+    string2=i + string2
+print(string2)
