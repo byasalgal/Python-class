72a4cb671d95fd0140453b7105aa925ae26dd296
byasalgal <122166499+byasalgal@users.noreply.github.com>
2025-01-14 19:47:55 +0800

    Add files via upload

---
+++ nested loops.py
@@ -0,0 +1,14 @@
+# for i in range(5):
+#     for j in range(i):
+#         print("⭐" , end="")
+
+#     print("")
+for i in range(10,0,-1):
+    # for l in range(-10,0,1):
+    #     print(" " , end="")
+    for j in range(i):
+        print("⭐" , end="")
+    for m in range(i-j):
+        print("Asd" , end="")
+
+    print("")
