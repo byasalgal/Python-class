4f67626c7794d322f39c3f45526d4a2f74c311a1
byasalgal <122166499+byasalgal@users.noreply.github.com>
2025-02-07 19:10:40 +0800

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+# # das = 9999
+# try:
+#     print(das)
+# except:
+#     print("new")
+# finally:
+#     print("comp")
+try:
+    kinj = int(input("enter number: "))
+except ValueError:
+    print("incorrect number or error")
+    print("Couldnt procced  likely due to  an error")
