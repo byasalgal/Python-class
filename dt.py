a1938bf10f05bec390fd18fb4efa3532dfa1e8b2
byasalgal <122166499+byasalgal@users.noreply.github.com>
2025-02-18 19:08:12 +0800

    Add files via upload

---
+++ dt.py
@@ -0,0 +1,18 @@
+# import datetime
+# # while True:
+# #     dt = datetime.datetime.now()
+# #     print(dt.strftime("%D:%Y"))
+import calendar
+y = 2025
+m = 2
+print(calendar.calendar(2021))
+# import random
+# dt = input("enter date like this one for example: 16:09: ")
+# while True:
+#     time = datetime.datetime.now()
+#     time = time.strftime("%H:%M")
+#     if str(time) == dt:
+#         print("its time")
+#         break
+#     else:
+#         pass
