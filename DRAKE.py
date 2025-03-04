8281c5c45d842053accd33d9747ca041c1d70d76
byasalgal <122166499+byasalgal@users.noreply.github.com>
2025-03-04 18:53:14 +0800

    Add files via upload

--- DRAKE.py
+++ DRAKE.py
@@ -1,17 +1,11 @@
-def find_squares():
-    try:
-        start = int(input("Enter the start of the range: "))
-        end = int(input("Enter the end of the range: "))
-        
-        squared_values = [num ** 2 for num in range(start, end + 1)]
-        
-        odd_squares = [sq for sq in squared_values if sq % 2 != 0]
-        even_squares = [sq for sq in squared_values if sq % 2 == 0]
+lisk = [1,999,324,567,7832]
+asd = []
+dsd = []
+for i in range(len(lisk)):
+    print(lisk[i])
+    if lisk[i] % 2 == 0:
+        asd.append(lisk[i])
+    else:
+        dsd.append(lisk[i])
 
-        print("Odd square values:", odd_squares)
-        print("Even square values:", even_squares)
-    
-    except ValueError:
-        print("Invalid input! Please enter valid integers.")
-
-find_squares()
+print(f"even: {asd} \n odd: {dsd}")
