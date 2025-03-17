55380808e0e529a4294dff5d8d9ba37668488d75
byasalgal <122166499+byasalgal@users.noreply.github.com>
2025-03-17 19:09:51 +0800

    symmetry difference added

---
+++ symmetrydif.py
@@ -0,0 +1,8 @@
+set1_A = {'blue', 'green'}
+set2_A = {'blue', 'yellow'}
+set1_B = {1, 2, 3, 4, 5}
+set2_B = {1, 5, 6, 7, 8, 9}
+sym_diff_A = set1_A.symmetric_difference(set2_A)
+sym_diff_B = set1_B.symmetric_difference(set2_B)
+print("symmetric difference for A:", sym_diff_A)
+print("symmetric difference for B:", sym_diff_B)
